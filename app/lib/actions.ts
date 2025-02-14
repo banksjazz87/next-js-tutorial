cb6b429ee69702fb36ecb6b2605db3dc6bab536f
Chris <banksjazz87@gmail.com>
2025-02-14 15:51:45 -0500

    tutorial: finished chapter 13

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -26,10 +26,15 @@ export async function createInvoice(formData: FormData) {
     const amountInCents = amount * 100;
     const date = new Date(0).toISOString().split('T')[0];
 
-    await sql`
+    try {
+         await sql`
         INSERT INTO invoices (customer_id, amount, status, date)
         VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
+
+    } catch (error) {
+        console.error(error);
+    }
 
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
@@ -47,19 +52,30 @@ export async function updateInvoice(id: string, formData: FormData) {
     status: formData.get('status'),
   });
  
-  const amountInCents = amount * 100;
- 
-  await sql`
-    UPDATE invoices
-    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-    WHERE id = ${id}
-  `;
+    const amountInCents = amount * 100;
+    
+    try {
+         await sql`
+            UPDATE invoices
+            SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+            WHERE id = ${id}
+        `;
+
+    } catch (error) {
+        console.error(error);
+    }
  
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
 
 export async function deleteInvoice(id: string) {
-	await sql`DELETE FROM invoices WHERE id = ${id}`;
-	revalidatePath("/dashboard/invoices");
+    try {
+        await sql`DELETE FROM invoices WHERE id = ${id}`;
+
+    } catch (error) {
+        console.error(error);
+    }
+
+    revalidatePath("/dashboard/invoices");
 }
